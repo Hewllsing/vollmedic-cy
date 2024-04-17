433ac12ffe92b45fa0a66d16bbecff9759b060b1
camilapessoa <camilabentespessoa@gmail.com>
2024-04-17 08:16:05 -0300

    video 3.5

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000/',
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    },
+
+  },
+
+});

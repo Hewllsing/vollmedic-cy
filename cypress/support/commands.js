3980276f0baea11de44ded469990626eac180b52
camilapessoa <camilabentespessoa@gmail.com>
2024-04-22 09:12:33 -0300

    video 2.1 Recuperando sessoes com o cy.session

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,13 @@
 Cypress.Commands.add('login', (email, senha) => {
-    cy.visit('/login')
-    cy.get('[data-test="inputLoginEmail"]').type(email)
-    cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false })
-    cy.get('[data-test="botaoTeste"]').should('be.visible').click()
-
+    cy.session([email, senha], () => {
+        cy.visit('/login')
+        cy.get('[data-test="inputLoginEmail"]').type(email)
+        cy.get('[data-test="inputLoginSenha"]').type(senha, { log: false })
+        cy.get('[data-test="botaoTeste"]').should('be.visible').click()
+        cy.location('pathname').should('eq', '/dashboard')
+    })
 })
+
 
 
 Cypress.Commands.add('cadastraEspecialista', (nome, email, senha, especialidade, crm, imagem, cep, rua, numero, complemento, estado) => {

668b80d7547e87877d390f86da731d12101a86fe
camilapessoa <camilabentespessoa@gmail.com>
2024-04-22 09:30:49 -0300

    Video 2.3 Validacoes em seletores nao deterministicos

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -3,16 +3,29 @@ describe('Usuário logado na página de dashboard', () => {
         cy.login(Cypress.env('email'), Cypress.env('senha'))
     })
 
-    it('Verifica página de redirecionamento no login com sucesso', () => {
-        cy.visit('/dashboard')
-        cy.url().should('eq', 'http://localhost:3000/dashboard')
+    context('Redirecionamento na página de dashboard', () => {
+        it('Verifica página de redirecionamento no login com sucesso', () => {
+            cy.visit('/dashboard')
+            cy.url().should('eq', 'http://localhost:3000/dashboard')
+        })
+
+        it('Com o usuário logado, cadastra um especialista', () => {
+            cy.visit('/dashboard')
+            cy.url().should('eq', 'http://localhost:3000/dashboard')
+            cy.contains('Cadastrar especialista').should('be.visible').click()
+
+
+        })
+
     })
 
-    it('Com o usuário logado, cadastra um especialista', () => {
-        cy.visit('/dashboard')
-        cy.url().should('eq', 'http://localhost:3000/dashboard')
-        cy.contains('Cadastrar especialista').should('be.visible').click()
-    })
+    context('Modal de cadastro de especialista', () => {
+        it('Verifica se o checkbox "Atende por plano?" está desmarcado', () => {
+            cy.visit('/dashboard')
+            cy.contains('Cadastrar especialista').should('be.visible').click()
+            cy.get('[type="checkbox"]').should('have.attr', 'aria-label', 'Atende por plano?').and('not.be.checked')
+        })
 
+    })
 
 })

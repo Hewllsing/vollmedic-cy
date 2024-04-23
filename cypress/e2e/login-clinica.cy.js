c44ab725e421ae1824453b1d8dae8f482fc4fc30
camilapessoa <camilabentespessoa@gmail.com>
2024-04-23 07:55:35 -0300

    Preparando ambiente para aula 4

--- cypress/e2e/login-clinica.cy.js
+++ cypress/e2e/login-clinica.cy.js
@@ -3,8 +3,9 @@ describe('Página de Login', () => {
         cy.visit('')
         cy.contains('Entrar').click()
     })
-    it('Digita email e senha corretos para efetuar o login', () => {
-        cy.login(Cypress.env('email'), Cypress.env('senha'))
+    it('Digita email e senha corretos para efetuar o login', { browser: 'firefox' }, () => {
+        cy.login(Cypress.env('email'), Cypress.env('senha'));
+        cy.url().should('eq', 'http://localhost:3000/dashboard');
 
     })
 })
